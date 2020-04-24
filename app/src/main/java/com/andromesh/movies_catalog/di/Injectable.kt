220a722184d809ef62d0336802b95c8876fcc63c
ume7715 <ume7715@gmail.com>
2020-04-24 12:20:48 +0530

    Initial commit

---
+++ app/src/main/java/com/andromesh/movies_catalog/di/Injectable.kt
@@ -0,0 +1,6 @@
+package com.andromesh.movies_catalog.di
+
+/**
+ * Marks an activity / fragment injectable.
+ */
+interface Injectable
---
+++ app/src/main/java/com/andromesh/movies_catalog/util/Constants.kt
@@ -0,0 +1,8 @@
+
+
+package com.andromesh.movies_catalog.util
+
+/**
+ * Constants used throughout the app.
+ */
+const val DATA_FILENAME = "sets.json"
